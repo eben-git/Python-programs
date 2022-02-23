e1597f3d0552b8eed04e380cdc3bd15ef43508db
unknown <eben.git@gmail.com>
2022-02-23 20:06:49 +0530

    23/2/22

---
+++ venv/studentclass.py
@@ -0,0 +1,7 @@
+class Student:
+
+    def __init__(self, name, age, grade, is_absent):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.is_absent = is_absent
---
+++ venv/studentobject.py
@@ -0,0 +1,5 @@
+from studentclass import Student
+
+student1 = Student("Kim", 18, "A+", False)
+
+print(student1.name)
